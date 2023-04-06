902de0a2f8ef7eec4d728040b6e4a9bd8d1ba78c
Dastagir <dastagir2k@gmail.com>
2023-04-06 23:21:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./components/Navbar";
+import RecipeCard from "./components/RecipieCard";
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  
+  // search for the recipe
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data)
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes()
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  }
+
+  return (
+    <div className="container">
+      <h1>Recipe Box</h1>
+      <SearchBar
+        isLoading={isLoading}
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSubmit}
+      />
+      <div className="recipes">
+        
+        {recipes ? recipes.map(recipe => (
+          <RecipeCard
+             key={recipe.idMeal}
+             recipe={recipe}
+          />
+        )) : "sorry Not found!!!!"}
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+// import React from "react"
+
+// export default function Navbar(){
+//     return <h1>Navbar components</h1>
+// }
+
+import React from "react";
+
+const SearchBar = ({
+    handleSubmit,
+    query,
+    isLoading,
+    setQuery
+}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                value={query}
+                className="form-control"
+                placeholder="Search Recipe"
+                name="query"
+                disabled={isLoading}
+                onChange={(event) => setQuery(event.target.value)}
+            />   
+            <input
+                disabled={isLoading || !query}
+                type="submit"
+                className="btn"
+                value="Search"
+            />
+        </form>
+    )
+};
+
+export default SearchBar;
